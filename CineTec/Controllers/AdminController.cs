c63c3cd3469db3b23aeeb55c471ce946bc305139
Joel-Barboza <joelbp5631@gmail.com>
2026-04-12 18:07:22 -0600

    feat: admin login

---
+++ CineTec/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CineTec.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminloginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AdminloginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public IActionResult AuthAdmin([FromQuery] string user, [FromQuery] string pswrd)
+        {
+            // obtenidos de appsettings.json
+            string AdminUser = _config.GetValue<string>("AdminUser");
+            string AdminPswrd = _config.GetValue<string>("AdminPswrd");
+            Console.WriteLine(AdminUser + " " + AdminPswrd);
+            Console.WriteLine(user + " " + pswrd);
+            if (AdminPswrd == pswrd && user == AdminUser)
+            {
+                Console.WriteLine("si");
+                return Ok(true);
+            }
+            return Ok(false);
+        }
+    }
+}
